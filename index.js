3517271c5096fa04f5fc3547a64ecafc5b9739f4
Chad Killingsworth <ckillingsworth@jackhenry.com>
2024-06-09 14:35:02 -0500

    Update babylon and acorn parsers to support newer syntax

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// Patch babylon to support newer ECMAScript syntax
+const babylon = require('babylon');
+const originalParse = babylon.parse;
+babylon.parse = function parse(input, options) {
+  const newOptions = Object.assign({}, options);
+  newOptions.plugins = newOptions.plugins
+      .concat([
+        'optionalChaining',
+        'nullishCoalescingOperator',
+        'optionalCatchBinding',
+        'classProperties',
+        'logicalAssignment',
+      ]);
+  return originalParse.call(babylon, input, newOptions);
+};
+
 const extractExpressions = require('./lib/gulp-polymer-rename-extract');
 const removeExpressions = require('./lib/gulp-polymer-rename-remove');
 
